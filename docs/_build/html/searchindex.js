9776e4a556ac33b1f9af67e839a0378f47df9a0b
Alejandro <acaceres@hyperiongray.com>
2014-09-28 17:29:31 -0400

    added sphinx docs

--- docs/_build/html/searchindex.js
+++ docs/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to sourcefinder&#8217;s documentation!"],objects:{},titleterms:{document:0,sourcefind:0,welcom:0,indic:0,tabl:0}})
+Search.setIndex({envversion:42,terms:{index:0,search:0,code:1,modul:0,ball:1,content:0,blah:[],tutori:[],page:0},objtypes:{},objnames:{},filenames:["index","tutorial"],titles:["Welcome to sourcefinder&#8217;s documentation!","Tutorial"],objects:{},titleterms:{sourcefind:0,welcom:0,indic:0,tutori:1,tabl:0,document:0}})
